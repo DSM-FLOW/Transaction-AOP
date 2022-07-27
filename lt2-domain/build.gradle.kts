2fd18962940c7beaf7655de40a8371a88ea79158
KimBeomJin <sdpthf@naver.com>
2022-07-27 17:17:35 +0900

    chore: all open

--- lt2-domain/build.gradle.kts
+++ lt2-domain/build.gradle.kts
@@ -1,12 +1,11 @@
 plugins {
-    id ("org.jetbrains.kotlin.plugin.allopen") version PluginVersions.ALLOPEN_VERSION
+    id("org.jetbrains.kotlin.plugin.allopen") version PluginVersions.ALLOPEN_VERSION
 }
 
 dependencies {
     implementation("org.springframework:spring-tx:5.3.20")
 }
 
-//allOpen {
-//    annotation("org.springframework.transaction.annotation.Transactional")
-//    annotation("com.lt2.lt2hexagonalspringserver.ApplicationService")
-//}
+allOpen {
+    annotation("com.lt2.lt2hexagonalspringserver.ApplicationService")
+}
